725f7962d00efceb7d7f3a7987f2accbb86e1e01
jason <jiang.hao@ikcrm.com>
2018-02-09 17:23:23 +0800

    path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
-// import Search from
+import Search from './components/search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        dede
+        <Search/>
       </div>
     );
   }
