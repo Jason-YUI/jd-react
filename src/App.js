dd46de3a596018f4f41fa54ba8dfa2a36286224c
jason <jiang.hao@ikcrm.com>
2018-02-09 15:20:17 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// import Search from
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        dede
+      </div>
+    );
+  }
+}
+
+export default App;
